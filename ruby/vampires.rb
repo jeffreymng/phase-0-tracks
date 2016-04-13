c28d7bc727757ef43626ab0546605b7743038219
Jeffrey Ng <jeffreymng@netzero.net>
2016-04-13 13:28:18 -0400

    added questions to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+puts  "What is your name?"
+  vampire_name = gets.chomp
+puts "How old are you? What year were you born?"
+  vampire_age = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  garlic_bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+  health_insurance = gets.chomp
