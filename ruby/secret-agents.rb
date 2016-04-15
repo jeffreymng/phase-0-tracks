f5111c7cbb470053c79289f5c1da6b5ce11b39f5
Jeffrey Ng <jeffreymng@netzero.net>
2016-04-15 19:15:52 -0400

    added decrypt method and tested

--- ruby/secret-agents.rb
+++ ruby/secret-agents.rb
@@ -14,3 +14,24 @@ end
 
 p encrypt("abc")
 p encrypt("zed")
+
+def decrypt(encoded_phrase)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  loop_counter = 0
+  decrypted_result = ""
+  while loop_counter < encoded_phrase.length
+    current_index = alphabet.index(encoded_phrase[loop_counter])
+    current_letter = encoded_phrase[loop_counter]
+    if current_letter == 'a'
+      decrypted_result += 'z'
+    else
+      previous_letter = alphabet[current_index - 1]
+      decrypted_result += previous_letter
+    end
+    loop_counter += 1
+  end
+  decrypted_result
+end
+
+p decrypt("bcd")
+p decrypt("afe")
