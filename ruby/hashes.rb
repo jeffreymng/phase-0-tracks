cf256020ca982bf35056fe315a4715d8fbc40407
Jeffrey Ng <jeffreymng@netzero.net>
2016-04-19 15:25:02 -0400

    added pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,12 @@
+# initialize a hash
+# ask the user for their name, age, # of children, decor theme, is their favorite color red, address, phone #, and if they like feng shui
+# store in symbols
+# convert input to appropriate data type
+# print hash out back to user when they are finished
+# allow user to change any incorrect key
+# if none, skip and print again
+# else ask what needs to be changed
+# convert gets.chomp string to symbol
+# call symbol and change it to new value
+# print hash
+
