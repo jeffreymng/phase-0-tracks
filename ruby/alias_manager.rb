84c33c299dd079809666ab67828b65f630e22ccc
Jeffrey Ng <jeffreymng@netzero.net>
2016-04-22 14:41:19 -0400

    added pseudocode to create method

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,11 @@
+# define a method that swaps the first and last name, changes all vowels to next vowel, and changes all consonants to next consonant
+# define variables for vowels and consonants for .index use
+# define a method that takes one argument (a string)
+# change all letters in string to lowercase
+# separate first and last name using .split(' ') and reorder them
+# separate each letter in the string into an array using .split or .chars
+# use .each to compare each letter in the array to vowels and consonants
+# advance vowels one letter forward and advance consonants one letter forward
+# create conditional statements for letters 'u' and 'z' to advance to 'a' and 'b', respectively, to deal with edge case
+# put the letters back together to form a name
+# capitalize the new name
