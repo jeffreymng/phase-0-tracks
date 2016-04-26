85581ad493e180664a5286f8f10b476b3efc956d
Jeffrey Ng <jeffreymng@netzero.net>
2016-04-26 11:52:40 -0400

    Added instance methods and tests

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,26 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    num.times {puts "Woof!"}
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(age)
+    age * 7
+  end
+
+  def play_dead
+    puts "lays down and doesn't move for 20 seconds"
+  end
 end
 
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(3)
+fido.roll_over
+p fido.dog_years(3)
+fido.play_dead
