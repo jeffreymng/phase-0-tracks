fbf4d0c6613e70c1e5bf11e2f18b26fb8d79b50f
Jeffrey Ng <jeffreymng@netzero.net>
2016-04-26 11:55:15 -0400

    created new class and methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -32,3 +32,38 @@ fido.speak(3)
 fido.roll_over
 p fido.dog_years(3)
 fido.play_dead
+
+class Soccer_Player
+
+  def initialize
+    puts "new player joins the team"
+  end
+
+  def kick
+    puts "kicks the ball"
+  end
+
+  def pass
+    puts "passes the ball"
+  end
+
+  def steal
+    puts "steals the ball"
+  end
+
+end
+
+soccer_team = []
+for i in 1 .. 50
+  player = Soccer_Player.new
+  soccer_team << player
+end
+
+puts soccer_team
+
+
+soccer_team.each do |member|
+  member.kick
+  member.pass
+  member.steal
+end
