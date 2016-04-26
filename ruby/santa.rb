9d5a9b9a7e109da1286aa2c59a133a94726c38e0
Jeffrey Ng <jeffreymng@netzero.net>
2016-04-26 14:29:03 -0400

    added attribute changing methods and getter/setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,5 @@
 class Santa
+
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
     @gender = gender
@@ -6,12 +7,35 @@ class Santa
     @age = 0
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
+
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type} cookie!"
   end
+
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at
+    @reindeer_ranking.rotate!
+  end
+
+  def change_gender=(new_gender)
+    @gender = new_gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
 end
 
 nick = Santa.new("gender", "ethnicity")
