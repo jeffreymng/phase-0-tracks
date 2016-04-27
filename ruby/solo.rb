36f58ea1718e6f6b0a60c2002472fe4798557265
Jeffrey Ng <jeffreymng@netzero.net>
2016-04-27 15:15:54 -0400

    defined class and methods and attributes

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -13,3 +13,39 @@
 #Bloodthirsty
 #Howls at the moon often
 #Hates Silver
+
+class Werewolf
+
+  attr_reader :sharp_claws, :enhanced_body
+  attr_accessor :fur_color, :fang_size, :age
+
+  def initialize(age, fur, fang)
+    puts "Bitten by a werewolf."
+    @age = age
+    @fur_color = fur
+    @fang_size = fang
+    @enhanced_body = "This is a very strong werewolf."
+    @sharp_claws = "The werewolf unsheaths its claws."
+  end
+
+  def transform(form)
+    if form == "human"
+      puts "The werewolf is currently in human form."
+    else
+      puts "The werewolf is currently in wolf form."
+    end
+  end
+
+  def howl
+    puts "AWHOOOOOOOOOOOOOOOO"
+  end
+
+  def hungry
+    puts "The werewolf stares at you and hungrily licks its lips."
+  end
+
+  def silver
+    puts "*whimper*"
+  end
+
+end
