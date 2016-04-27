990cf5618324f3e27b03f2d9c16717156749b345
Jeffrey Ng <jeffreymng@netzero.net>
2016-04-27 16:00:22 -0400

    added loop for user creation of werewolves

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -7,6 +7,7 @@
 #Fur Color: Varies
 #Fang Size: Varies
 #Age: Varies
+#Name: Varies
 
 #Behavior-----------------
 #Able to transform into a human or Werewolf
@@ -17,22 +18,21 @@
 class Werewolf
 
   attr_reader :sharp_claws, :enhanced_body
-  attr_accessor :fur_color, :fang_size, :age
+  attr_accessor :fur_color, :fang_size, :age, :name
 
-  def initialize(age, fur, fang)
+  def initialize(name, age, fur, fang)
     puts "Bitten by a werewolf."
+    @name = name
     @age = age
     @fur_color = fur
     @fang_size = fang
-    @enhanced_body = "This is a very strong werewolf."
-    @sharp_claws = "The werewolf unsheaths its claws."
   end
 
   def transform(form)
     if form == "human"
-      puts "The werewolf is currently in human form."
+      puts "#{@name} is currently in human form."
     else
-      puts "The werewolf is currently in wolf form."
+      puts "#{@name} is currently in wolf form."
     end
   end
 
@@ -41,11 +41,41 @@ class Werewolf
   end
 
   def hungry
-    puts "The werewolf stares at you and hungrily licks its lips."
+    puts "#{@name} stares at you and hungrily licks their #{@fang_size} fangs."
   end
 
   def silver
-    puts "*whimper*"
+    puts "*#{@name} cowers and whimpers in fear.*"
   end
 
+end
+
+werewolves = []
+
+end_program = false
+until end_program
+  puts "Let's create a werewolf! Press 'Enter' to continue and type 'done' when you've created enough."
+    done = gets.chomp.downcase
+      if done == "done"
+        end_program = true
+      else
+        puts "What is its name?"
+          name = gets.chomp
+        puts "How old are they?"
+          age = gets.chomp.to_i
+        puts "What color is their fur?"
+          fur_color = gets.chomp
+        puts "How large are its fangs?"
+          fangs = gets.chomp
+        current_werewolf = Werewolf.new(name, age, fur_color, fangs)
+        werewolves << current_werewolf
+      end
+end
+
+werewolves.each do |werewolf|
+  puts "This werewolf's name is #{werewolf.name}."
+  puts "#{werewolf.name} is #{werewolf.age} years old and has #{werewolf.fur_color} fur."
+  puts "#{werewolf.name}'s fangs are #{werewolf.fang_size}."
+  werewolf.hungry
+  werewolf.howl
 end
