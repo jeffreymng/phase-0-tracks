0c27812aad282a11c01fba8b34596935f878536e
Jeffrey Ng <jeffreymng@netzero.net>
2016-04-27 14:23:16 -0400

    Designed a werewolf class

---
+++ ruby/solo.rb
@@ -0,0 +1,15 @@
+#Species------------------
+#Werewolf (Lycanthrope)
+
+#Characteristics----------
+#Sharp Claws
+#Enhanced Strength, Speed, and Smell
+#Fur Color: Varies
+#Fang Size: Varies
+#Age: Varies
+
+#Behavior-----------------
+#Able to transform into a human or Werewolf
+#Bloodthirsty
+#Howls at the moon often
+#Hates Silver
