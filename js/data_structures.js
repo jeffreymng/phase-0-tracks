076f262a8c4f4be6d27f9aa15023e5a2a1d46431
Jeffrey Ng <jeffreymng@netzero.net>
2016-05-04 18:24:47 -0400

    created two arrays and added data to them

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "yellow", "green", "purple"];
+var horseNames = ["sparkles", "george", "jerry", "mary"];
+
+colors.push("pink");
+horseNames.push("gary");
+
+console.log(colors);
+console.log(horseNames);
