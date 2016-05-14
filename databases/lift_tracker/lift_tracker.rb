f3b8a2d2a097daadc2df5332663c299b1dd45dd6
Jeffrey Ng <jeffreymng@netzero.net>
2016-05-14 18:20:51 -0400

    added 2nd to recall all lifts on certain date

--- databases/lift_tracker/lift_tracker.rb
+++ databases/lift_tracker/lift_tracker.rb
@@ -7,7 +7,7 @@
   # Assuming 3 sets performed for each exercise
   # Number of reps
   # Weight lifted for each set
-# Create method to call information from database based on date
+# Create methods to call information from database
 
 require 'sqlite3'
 
@@ -46,26 +46,48 @@ lifts.execute("INSERT OR IGNORE INTO bodypart (name) VALUES ('leg');")
 
 def add_exercise(lifts)
 
-  puts "Date of exercise?"
+  puts "Date of exercise? (MM/DD/YYYY)"
     exercise_date = gets.chomp
   puts "What did you work on? (Enter '1' for upper body, '2' for back, or '3' for legs.)"
     body_focus = gets.chomp.to_i
+  until body_focus == 1 || body_focus == 2 || body_focus == 3
+    puts "That is not a valid answer. Please enter '1' for upper body, '2' for back, or '3' for legs."
+      body_focus = gets.chomp.to_i
+  end
   puts "What exercise did you perform?"
     exercise = gets.chomp
-  puts "First Set: How many reps?"
+  puts "First Set: How many reps? (Numbers Only)"
     reps1 = gets.chomp.to_i
-  puts "First Set: How much weight did you use?"
+  puts "First Set: How many pounds did you use? (Numbers Only)"
     weight1 = gets.chomp.to_i
-  puts "Second Set: How many reps?"
+  puts "Second Set: How many reps? (Numbers Only)"
     reps2 = gets.chomp.to_i
-  puts "Second Set: How much weight did you use?"
+  puts "Second Set: How many pounds did you use? (Numbers Only)"
     weight2 = gets.chomp.to_i
-  puts "Third Set: How many reps?"
+  puts "Third Set: How many reps? (Numbers Only)"
     reps3 = gets.chomp.to_i
-  puts "Third Set: How much weight did you use?"
+  puts "Third Set: How many pounds did you use? (Numbers Only)"
     weight3 = gets.chomp.to_i
 
   lifts.execute("INSERT INTO lifts (lift_date, bodypart_id, exercise, reps1, weight1, reps2, weight2, reps3, weight3) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [exercise_date, body_focus, exercise, reps1, weight1, reps2, weight2, reps3, weight3])
 
 end
 
+def lifts_on_date(lifts, date)
+  puts "Here are all your lifts for #{date}."
+  on_date = lifts.execute("SELECT lifts.exercise, lifts.reps1, lifts.weight1, lifts.reps2, lifts.weight2, lifts.reps3, lifts.weight3, bodypart.name FROM lifts JOIN bodypart ON lifts.bodypart_id = bodypart.id WHERE lift_date='#{date}';")
+  on_date.each do |lifts|
+    puts "Lift: #{lifts['exercise']}"
+    puts "1st Set: #{lifts['reps1']} x #{lifts['weight1']} lbs"
+    puts "2nd Set: #{lifts['reps2']} x #{lifts['weight2']} lbs"
+    puts "3rd Set: #{lifts['reps3']} x #{lifts['weight3']} lbs"
+  end
+end
+
+puts "Welcome to the lift tracker. Please type 'help' for a list of commands or, if you already know the commands, type in the command you would like to execute."
+
+
+
+# DRIVER CODE
+# add_exercise(lifts)
+lifts_on_date(lifts, "11/11/1111")
