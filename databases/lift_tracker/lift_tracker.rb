8f647b5b64484fd12197fa301dd0a6e549fb8344
Jeffrey Ng <jeffreymng@netzero.net>
2016-05-14 15:34:39 -0400

    added comments. what i want my program to do.

---
+++ databases/lift_tracker/lift_tracker.rb
@@ -0,0 +1,10 @@
+# Create a program to keep track of what lifts you performed on what dates.
+# Store all info in a database
+# Ask user for input
+  # Date
+  # What part of the body was worked on (upper body, back, legs) store in database as foreign key. Create separate database for this.
+  # What exercises were performed
+  # Number of sets
+  # Number of reps
+  # Weight lifted for each
+# Create method to call information from database based on date
