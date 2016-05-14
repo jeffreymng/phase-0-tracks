a5eda26c458f50c5a6e39d4cb9a8df2b4c51539f
Jeffrey Ng <jeffreymng@netzero.net>
2016-05-14 15:58:39 -0400

    added create table command for sql

--- databases/lift_tracker/lift_tracker.rb
+++ databases/lift_tracker/lift_tracker.rb
@@ -6,5 +6,23 @@
   # What exercises were performed
   # Number of sets
   # Number of reps
-  # Weight lifted for each
+  # Weight lifted for each set
 # Create method to call information from database based on date
+
+require 'sqlite3'
+
+lifts = SQLite3::Database.new("lifts.db")
+lifts.result_as_hash = true
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS lifts(
+    id INTEGER PRIMARY KEY,
+    lift_date VARCHAR(255),
+    bodypart_id INT,
+    exercise VARCHAR(255),
+    sets INT,
+    reps INT,
+    weight INT,
+    FOREIGN KEY (bodypart_id) REFERENCES bodypart(id),
+  )
+SQL
